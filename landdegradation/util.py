057ef4cb19c699d3e16613abb8832f91b4d565d2
Grace Amondi <ecofluff990@gmail.com>
2020-10-12 15:09:19 +0300

    Update util.py

--- landdegradation/util.py
+++ landdegradation/util.py
@@ -207,7 +207,7 @@ class TEImage(object):
         logger.debug('{}'.format(urls))
         gee_results = CloudResults(task_name,
                                    self.band_info,
-                                   thumbUrl)
+                                   urls)
         results_schema = CloudResultsSchema()
         json_results = results_schema.dump(gee_results)
 
