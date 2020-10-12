7be681d4799b32571caf0386608737a7a61807c5
Grace Amondi <ecofluff990@gmail.com>
2020-10-12 12:25:28 +0300

    Update util.py

--- landdegradation/util.py
+++ landdegradation/util.py
@@ -188,14 +188,14 @@ class TEImage(object):
         
         logger.debug("{}".format(self.image.getThumbURL({'region':get_coords(geojson),'dimensions': 2058,'format':'geotiff'})))
         
-        thumbUrl = {
+        thumbUrl = [{
             "md5Hash": "3x56NWs06AVP7fdd45p1cw==",
             "url": '{}'.format(self.image.getThumbURL({
                 'region':get_coords(geojson),
                 'dimensions': 2058,
                 'format':'geotiff'
                 }))
-        }
+        }]
         
         urls = []
         for task in tasks:
