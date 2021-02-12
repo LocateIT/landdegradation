9548e3c270a37d9696ae043fbac897d428c1fe57
unknown <Grace-Amondi>
2021-02-12 10:52:59 +0300

    change rock fragment data source

--- landdegradation/soil_quality.py
+++ landdegradation/soil_quality.py
@@ -50,26 +50,7 @@ def soil_quality(depth, texture_matrix, geometry, EXECUTION_ID, logger):
     # ==========================
     slope = ee.Image("users/miswagrace/slope_north_africa").clip(geometry)
     slope = slope.reproject(crs=srtm.projection())
-    # slope = ee.Terrain.slope(srtm).clip(geometry)
-
-    # # convert slope to radians
-    # slopeRad = slope.multiply(0.0174533)
-
-    # # convert slope radians to percentage
-    # slopePercent = slopeRad.atan().multiply(100)
-
-    # # reclassify slope according to ranges
-    # slopeClass = ee.Image(-32768) \
-    #     .where(slopePercent.lt(6), 1) \
-    #     .where(slopePercent.gte(6).And(slopePercent.lte(18)), 1.2) \
-    #     .where(slopePercent.gte(18).And(slopePercent.lte(35)), 1.5) \
-    #     .where(slopePercent.gt(35), 2) \
-    #     .rename("Slope Class")
-
-    # # remove no data values
-    # slopeClass = slopeClass.where(slopeClass.eq(9999), -32768)
-    # slopeClass = slopeClass.updateMask(slopeClass.neq(-32768))
-
+   
     # ==========================
     # TEXTURE
     # ==========================    
@@ -106,27 +87,18 @@ def soil_quality(depth, texture_matrix, geometry, EXECUTION_ID, logger):
     # ROCK FRAGMENT
     # ==========================
     # classify rock fragment based on 3 classes
-    rock_fragment = ee.Image("users/miswagrace/rock_fragments")
+    rock_fragment = ee.Image("users/miswagrace/rock_fragment_north_africa")
     fragmentClass = rock_fragment \
         .where(rock_fragment.lt(20), 2) \
         .where(rock_fragment.gte(20).And(rock_fragment.lte(60)), 1.3) \
         .where(rock_fragment.gt(60), 1) \
         .rename("Rock Fragments")
 
-    fragmentClass = fragmentClass.unmask(-32768)
-    fragmentClass = fragmentClass.where(fragmentClass.eq(-32768), 2)
-    
     # ==========================
     # SOIL DRAINAGE
     # ==========================
-    # classify rock fragment based on 3 classes
-    # drainage = ee.Image("users/miswagrace/soil_drainage")
-    # soil_drainage = ee.Image(-32768) \
-    #     .where(drainage.lte(2), 2) \
-    #     .where(drainage.eq(3), 1.2) \
-    #     .where(drainage.gt(3), 1) \
-    #     .rename("Soil Drainage")
-    drainage = ee.Image("users/miswagrace/drainage_north_africa")
+ 
+    drainage = ee.Image("users/miswagrace/drainage_northafrica")
     soil_drainage = drainage.remap([1,2,3,4,5,6,7],[2.0, 2.0, 1.4, 1.2, 1.0, 1.7, 2.0])
     # ==========================
     # SOIL QUALITY INDEX
@@ -152,67 +124,3 @@ def soil_quality(depth, texture_matrix, geometry, EXECUTION_ID, logger):
     return TEImage(sqi,
         [BandInfo("Soil Quality Index (cm deep)", add_to_map=True, metadata={'depth':depth})])
 
-
-# def soil_quality(depth, geometry, EXECUTION_ID,logger):
-    # """
-    # ===========================================================================================
-    #              SOIL QUALITY INDEX (CQI)
-    # ===========================================================================================
-    # The impact of the soil factor to the process of desertification is determined by the strength of cohesion between soil particles, water retention ability of the soil, soil texture, and structure. The soil quality index (SQI), developed by OSS, that will be used is based on four parameters:
-
-    # - parent material
-    # - soil depth (0 to 200cm)
-    # - soil texture
-    # - slope
-    # - rock fragment
-    # - drainage
-
-    # The formula used to compute the SQI from the above-mentioned parameters is as shown below:
-
-    # SQI = (parent material*soil depth*soil texture* slope* rock fragment*drainage)^1/6
-    # """
-
-    # logger.debug("Entering soil quality function.")
-
-    # # define slope 
-    # srtm = ee.Image("USGS/SRTMGL1_003")
-
-    # slope = ee.Terrain.slope(srtm).clip(geometry)
-
-    # # define soil texture 
-    # texture = ee.Image("OpenLandMap/SOL/SOL_TEXTURE-CLASS_USDA-TT_M/v02").clip(geometry)
-
-    # # select texture based on depth 
-    # if depth < 10:
-    #     texture = texture.select(['b0'])
-    # elif depth >= 10 or depth < 30:
-    #     texture = texture.select(['b10'])
-    # elif depth >= 30 or depth < 60:
-    #     texture = texture.select(['b30'])
-    # elif depth >= 60 or depth < 100:
-    #     texture = texture.select(['b60'])
-    # elif depth >= 100 or depth < 200:
-    #     texture = texture.select(['b100'])
-    # else:
-    #     texture = texture.select(['b200'])
-    
-
-    # # define soil parent material 
-    # lithology = ee.Image("users/miswagrace/global_lithology").clip(geometry).select('b1')
-
-    # # calculate SQI
-    # image = ee.Image()
-    # sqi = image.expression('(slope * parent_material * soil_texture * soil_depth) ** (1/4)', {
-    # 'slope':slope,
-    # 'parent_material':lithology,
-    # 'soil_texture':texture,
-    # 'soil_depth':depth
-    # }).rename('sqi')
-
-    # return TEImage(sqi,
-    #     [BandInfo("Soil Quality Index", add_to_map=True, metadata={'depth':depth})])
-
-
-
-
-    
